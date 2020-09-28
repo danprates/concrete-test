ffcf9f568dd08d22ec3e39adcd2eaf202e2c0130
Daniel Prates <danprates94@gmail.com>
2020-09-28 11:57:40 -0300

    test: auth controller

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,12 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
+
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
 
 describe('AuthController', () => {
   let controller: AuthController;
+  const authServiceMock = {
+    register: jest.fn(),
+    login: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
+      providers: [{ provide: AuthService, useFactory: () => authServiceMock }],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
@@ -14,5 +21,29 @@ describe('AuthController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('register', () => {
+    it('should pass correct params to dependencies', async () => {
+      authServiceMock.register.mockResolvedValueOnce(true);
+      const data = { name: 'foo', email: 'bar', password: '123456' };
+
+      const result = await controller.register(data);
+
+      expect(result).toBeTruthy();
+      expect(authServiceMock.register).toHaveBeenNthCalledWith(1, data);
+    });
+  });
+
+  describe('login', () => {
+    it('should pass correct params to dependencies', async () => {
+      authServiceMock.login.mockResolvedValueOnce(true);
+      const req = { user: { userId: 'foo' } };
+
+      const result = await controller.login(req);
+
+      expect(result).toBeTruthy();
+      expect(authServiceMock.login).toHaveBeenNthCalledWith(1, req.user);
+    });
   });
 });
